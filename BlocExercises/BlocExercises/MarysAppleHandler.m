ad38e744806ea64ce86e80b93a7b5d43b27ff7ef
Thomas Steimann <thomas.steimann@resolvex.ch>
2014-10-11 01:24:34 +0200

    tux007: Completed if, else, tenary

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -15,6 +15,18 @@
 
     /* WORK HERE */
     
+     if (dollars < 5) {
+         itemToReturn =@"get out of my store";
+     } else if (dollars >= 5 && dollars < 6) {
+         itemToReturn =@"have some gum";
+     } else if (dollars >= 6 && dollars < 1000){
+         itemToReturn =@"have an apple";
+     } else if (dollars >= 1000 && dollars < 1000000){
+         itemToReturn =@"have an Apple computer";
+     } else if (dollars >= 1000000){
+         itemToReturn =@"have The Big Apple";
+     }
+    
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
@@ -24,11 +36,8 @@
 
     NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
+    return self.getsDiscount ? cost *= .75 : cost;
     
-    return cost;
 }
 
 @end
