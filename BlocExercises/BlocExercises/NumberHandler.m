f177c66af543c33db04dfa0aad404b7ea428889d
Thomas Steimann <thomas.steimann@resolvex.ch>
2014-10-15 16:55:55 +0200

    tux007: Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,49 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    // int firstNu = [number intValue];
+    // int secondNu = 2;
+    // NSNumber *result = [NSNumber numberWithInt:firstNu *secondNu];
+    // return result;
+    
+    NSNumber *numberDouble = [NSNumber numberWithInt:([number intValue] * 2)];
+    return numberDouble;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *answerArray = [NSMutableArray array];
+    
+    if (number < otherNumber) {
+        for(NSInteger i = number; i <= otherNumber; i++) {
+            NSNumber *nextNumber = [NSNumber numberWithInteger:i];
+            [answerArray addObject: nextNumber];
+            }
+        } else {
+            for(NSInteger i = otherNumber; i <= number; i++) {
+                NSNumber *nextNumber = [NSNumber numberWithInteger:i];
+                [answerArray addObject: nextNumber];
+                }
+            }
+    return answerArray;
+    
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    
+    NSInteger lowNu = [arrayOfNumbers[0] integerValue];
+    
+    for (NSInteger i = 0; i < [arrayOfNumbers count]; i++) {
+        if (lowNu > [arrayOfNumbers[i] integerValue])
+            {
+                lowNu = [arrayOfNumbers[i] integerValue];
+                }
+        }
+    
+    return lowNu;
+    
 }
 
 @end
